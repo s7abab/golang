9f33cde0e48448c80a1d6d7c775127fec4c2c251
Shabab <s7abab2k1@gmail.com>
2024-03-04 22:41:46 +0530

    first commit

---
+++ 02variable/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "Jhon"
+	fmt.Println(username)
+	fmt.Printf("Variable is type: %T \n", username)
+
+	var isLoggedIn bool = true
+	fmt.Printf("fsd %T \n", isLoggedIn)
+}
